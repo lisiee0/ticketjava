3efd7e52f6917d3783b33db06a5f0913ef70362e
copymachine <copymach@naver.com>
2022-03-03 16:39:33 +0900

    메인컨트롤러 인덱스 부를 수 있게 수정

--- src/main/java/com/ticketjava/controller/MainController.java
+++ src/main/java/com/ticketjava/controller/MainController.java
@@ -2,14 +2,22 @@ package com.ticketjava.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+	
+	@RequestMapping(value="/", method={RequestMethod.GET, RequestMethod.POST })
+	public String main() {
+		System.out.println("MainController.main 메인 페이지");
+		return "main/index";
+	} // main
+	
 	@RequestMapping("search")
 	public String search() {
 		return "main/search";
-	}
+	} // search
 	
-}
+} // The end of MainController
