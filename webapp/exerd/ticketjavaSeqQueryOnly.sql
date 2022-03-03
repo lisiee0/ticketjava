86e3a025cf3d8c64aaab3882f3f82af1b00a14c2
copymachine <copymach@naver.com>
2022-03-03 16:19:38 +0900

    sql쿼리문 내용 수정 (드랍 부분을 위로)

--- webapp/exerd/ticketjavaSeqQueryOnly.sql
+++ webapp/exerd/ticketjavaSeqQueryOnly.sql
@@ -1,5 +1,26 @@
 /* ticketjavaSeqQueryOnly SQL 티켓자바 시퀀스 sql 쿼리 모음 */ 
 
+
+--자동생성 번호 꼬일때 초기화 (리셋)
+drop SEQUENCE seq_user_no;
+drop SEQUENCE seq_theater_no;
+drop SEQUENCE seq_hall_no;
+drop SEQUENCE seq_seat_no;
+drop SEQUENCE seq_prod_no;
+drop SEQUENCE seq_rez_no;
+drop SEQUENCE seq_dc_no;
+drop SEQUENCE seq_review_no;
+drop SEQUENCE seq_auth_no;
+drop SEQUENCE seq_noti_no;
+drop SEQUENCE seq_qna_no;
+drop SEQUENCE seq_qna_gno;
+drop SEQUENCE seq_notice_no;
+drop SEQUENCE seq_req_no;
+drop SEQUENCE seq_selseat_no;
+drop SEQUENCE seq_detail_no;
+drop SEQUENCE seq_seatprice_no;
+
+
 --티켓자바 모든 시퀀스 만들기
 create sequence seq_user_no increment by 1 START WITH 1 nocache;
 create sequence seq_theater_no increment by 1 START WITH 1 nocache;
@@ -23,26 +44,6 @@ create sequence seq_seatprice_no increment by 1 START WITH 1 nocache;
 
 --자동생성 번호, 시퀀스 조회 (서버내 모든 대상)
 SELECT * FROM USER_SEQUENCES;
-
-
---자동생성 번호 꼬일때 초기화 (리셋)
-drop SEQUENCE seq_user_no;
-drop SEQUENCE seq_theater_no;
-drop SEQUENCE seq_hall_no;
-drop SEQUENCE seq_seat_no;
-drop SEQUENCE seq_prod_no;
-drop SEQUENCE seq_rez_no;
-drop SEQUENCE seq_dc_no;
-drop SEQUENCE seq_review_no;
-drop SEQUENCE seq_auth_no;
-drop SEQUENCE seq_noti_no;
-drop SEQUENCE seq_qna_no;
-drop SEQUENCE seq_qna_gno;
-drop SEQUENCE seq_notice_no;
-drop SEQUENCE seq_req_no;
-drop SEQUENCE seq_selseat_no;
-drop SEQUENCE seq_detail_no;
-drop SEQUENCE seq_seatprice_no;
 
 
 --커밋 
