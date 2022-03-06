d996d8d7455ce5caf07d3a6c84a5576daaaccd60
구민석 <ser_om@naver.com>
2022-03-06 22:14:00 +0900

    BM

--- src/main/java/com/ticketjava/dao/BusinessDao.java
+++ src/main/java/com/ticketjava/dao/BusinessDao.java
@@ -15,5 +15,9 @@ public class BusinessDao {
 	public void insert(BusinessVo businessVo) {
 		sqlSession.insert("business.insert", businessVo);
 	}
+	
+	public void selectOne() {
+		return ;
+	}
 
 }
