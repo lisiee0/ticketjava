35e7de9aca8fc38cc0392ba605317df1a6931826
copymachine <copymach@naver.com>
2022-03-07 10:28:18 +0900

    예약vo 수정

--- src/main/java/com/ticketjava/vo/ReserveDetailVo.java
+++ src/main/java/com/ticketjava/vo/ReserveDetailVo.java
@@ -3,12 +3,11 @@ package com.ticketjava.vo;
 public class ReserveDetailVo {
 
 //	예매정보 상세용
-	
-	private int user_no; //회원번호
-	private int rez_no; // 예매번호
-	private int prod_no; // 공연번호
-	private int theater_no; // 공연장번호
-	private int selseat_no; // 선택좌석번호
+	private int userNo; //회원번호
+	private int rezNo; // 예매번호
+	private int prodNo; // 공연번호
+	private int theaterNo; // 공연장번호
+	private int selseatNo; // 선택좌석번호
 	private int grade; // 좌석등급 1vip 2r 3s 4a 5b
 	private int price; // 좌석 가격 
 	private int status; //상태 예매취소(1예매 0취소)
@@ -19,21 +18,20 @@ public class ReserveDetailVo {
 	private String theatername; // 장소 (예술의전당 오페라극장)
 	private String rezdate; // 예매일
 	private String viewdate; // 관람일
-	
-	
+
 	
 	public ReserveDetailVo() {
 	}
 	
 	
-	public ReserveDetailVo(int user_no, int rez_no, int prod_no, int theater_no, int selseat_no, int grade, int price,
+	public ReserveDetailVo(int userNo, int rezNo, int prodNo, int theaterNo, int selseatNo, int grade, int price,
 			int status, int ticketCount, String name, String prodname, String theatername, String rezdate,
 			String viewdate) {
-		this.user_no = user_no;
-		this.rez_no = rez_no;
-		this.prod_no = prod_no;
-		this.theater_no = theater_no;
-		this.selseat_no = selseat_no;
+		this.userNo = userNo;
+		this.rezNo = rezNo;
+		this.prodNo = prodNo;
+		this.theaterNo = theaterNo;
+		this.selseatNo = selseatNo;
 		this.grade = grade;
 		this.price = price;
 		this.status = status;
@@ -44,35 +42,35 @@ public class ReserveDetailVo {
 		this.rezdate = rezdate;
 		this.viewdate = viewdate;
 	}
-	public int getUser_no() {
-		return user_no;
+	public int getUserNo() {
+		return userNo;
 	}
-	public void setUser_no(int user_no) {
-		this.user_no = user_no;
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
 	}
-	public int getRez_no() {
-		return rez_no;
+	public int getRezNo() {
+		return rezNo;
 	}
-	public void setRez_no(int rez_no) {
-		this.rez_no = rez_no;
+	public void setRezNo(int rezNo) {
+		this.rezNo = rezNo;
 	}
-	public int getProd_no() {
-		return prod_no;
+	public int getProdNo() {
+		return prodNo;
 	}
-	public void setProd_no(int prod_no) {
-		this.prod_no = prod_no;
+	public void setProdNo(int prodNo) {
+		this.prodNo = prodNo;
 	}
-	public int getTheater_no() {
-		return theater_no;
+	public int getTheaterNo() {
+		return theaterNo;
 	}
-	public void setTheater_no(int theater_no) {
-		this.theater_no = theater_no;
+	public void setTheaterNo(int theaterNo) {
+		this.theaterNo = theaterNo;
 	}
-	public int getSelseat_no() {
-		return selseat_no;
+	public int getSelseatNo() {
+		return selseatNo;
 	}
-	public void setSelseat_no(int selseat_no) {
-		this.selseat_no = selseat_no;
+	public void setSelseatNo(int selseatNo) {
+		this.selseatNo = selseatNo;
 	}
 	public int getGrade() {
 		return grade;
@@ -128,15 +126,17 @@ public class ReserveDetailVo {
 	public void setViewdate(String viewdate) {
 		this.viewdate = viewdate;
 	}
+	
+	
 	@Override
 	public String toString() {
-		return "ReserveDetailVo [user_no=" + user_no + ", rez_no=" + rez_no + ", prod_no=" + prod_no + ", theater_no="
-				+ theater_no + ", selseat_no=" + selseat_no + ", grade=" + grade + ", price=" + price + ", status="
+		return "ReserveDetailVo [userNo=" + userNo + ", rezNo=" + rezNo + ", prodNo=" + prodNo + ", theaterNo="
+				+ theaterNo + ", selseatNo=" + selseatNo + ", grade=" + grade + ", price=" + price + ", status="
 				+ status + ", ticketCount=" + ticketCount + ", name=" + name + ", prodname=" + prodname
 				+ ", theatername=" + theatername + ", rezdate=" + rezdate + ", viewdate=" + viewdate + "]";
 	}
 	
-	
+		
 	
 	
 } // The end of ReserveDetailVo
