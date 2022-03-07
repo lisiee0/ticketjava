16570309d274304a0e8a8db23ae1328e9fa624f6
이원준 <ggoong8@gmail.com>
2022-03-07 17:52:03 +0900

    reser vo
    prod name 추가

--- src/main/java/com/ticketjava/vo/ReservationVo.java
+++ src/main/java/com/ticketjava/vo/ReservationVo.java
@@ -5,95 +5,117 @@ public class ReservationVo {
 	private int rezNo;
 	private int prodNo;
 	private int userNo;
-	private int ticketCount; // 예매 매수 
 
 	private String rezDate;
 	private String viewDate;
 	private String phone;
 	private String email;
 	private String status;
-	
-	
+	private int ticketCount; // 예매 매수
+	private String prodName;
+
 	public ReservationVo() {
 	}
-	
-	
-	public ReservationVo(int rezNo, int prodNo, int userNo, int ticketCount, String rezDate, String viewDate,
-			String phone, String email, String status) {
+
+	public ReservationVo(int rezNo, int prodNo, int userNo, String rezDate, String viewDate, String phone, String email,
+			String status, int ticketCount, String prodName) {
 		this.rezNo = rezNo;
 		this.prodNo = prodNo;
 		this.userNo = userNo;
-		this.ticketCount = ticketCount;
 		this.rezDate = rezDate;
 		this.viewDate = viewDate;
 		this.phone = phone;
 		this.email = email;
 		this.status = status;
+		this.ticketCount = ticketCount;
+		this.prodName = prodName;
 	}
-	
-	
+
 	public int getRezNo() {
 		return rezNo;
 	}
+
 	public void setRezNo(int rezNo) {
 		this.rezNo = rezNo;
 	}
+
 	public int getProdNo() {
 		return prodNo;
 	}
+
 	public void setProdNo(int prodNo) {
 		this.prodNo = prodNo;
 	}
+
 	public int getUserNo() {
 		return userNo;
 	}
+
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
-	public int getTicketCount() {
-		return ticketCount;
-	}
-	public void setTicketCount(int ticketCount) {
-		this.ticketCount = ticketCount;
-	}
+
 	public String getRezDate() {
 		return rezDate;
 	}
+
 	public void setRezDate(String rezDate) {
 		this.rezDate = rezDate;
 	}
+
 	public String getViewDate() {
 		return viewDate;
 	}
+
 	public void setViewDate(String viewDate) {
 		this.viewDate = viewDate;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
-	@Override
-	public String toString() {
-		return "ReservationVo [rezNo=" + rezNo + ", prodNo=" + prodNo + ", userNo=" + userNo + ", ticketCount="
-				+ ticketCount + ", rezDate=" + rezDate + ", viewDate=" + viewDate + ", phone=" + phone + ", email="
-				+ email + ", status=" + status + "]";
+
+	public int getTicketCount() {
+		return ticketCount;
 	}
 
-	
-	
+	public void setTicketCount(int ticketCount) {
+		this.ticketCount = ticketCount;
+	}
+
+	public String getProdName() {
+		return prodName;
+	}
+
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservationVo [rezNo=" + rezNo + ", prodNo=" + prodNo + ", userNo=" + userNo + ", rezDate=" + rezDate
+				+ ", viewDate=" + viewDate + ", phone=" + phone + ", email=" + email + ", status=" + status
+				+ ", ticketCount=" + ticketCount + ", prodName=" + prodName + "]";
+	}
+
 } // The end of ReservationVo
