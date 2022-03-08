3540b096ceabeb1d4f31f032452ffc329f881090
구민석 <ser_om@naver.com>
2022-03-08 10:46:27 +0900

    BM

--- src/main/java/com/ticketjava/service/BusinessService.java
+++ src/main/java/com/ticketjava/service/BusinessService.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,5 +56,9 @@ public class BusinessService {
 	}
 
 	// 공연 목록 불러오기
+	public List<BusinessVo> getList(){
+		return BusinessDao.getBmList();
+	}
+	
 
 }
