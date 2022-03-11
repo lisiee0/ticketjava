2adfad75215adc6d37881d2458688c8b13186883
y <y@DESKTOP-75E29QT>
2022-03-11 18:13:10 +0900

    취소 알림 순서

--- src/main/java/com/ticketjava/service/NotireqService.java
+++ src/main/java/com/ticketjava/service/NotireqService.java
@@ -81,4 +81,29 @@ public class NotireqService {
 			return "fail";
 	}
 	
+	
+	public void notiSend() {
+		/* 
+		 1. selseat 예매취소(selseatNo) -->  rezNo, grade, section, col, num
+		 ( selseat status = 0 )
+		
+		 2. reserve(rezNo) >  prodNo, userNo, viewDate
+		 ( rezNo 같은 selseat status가 모두 0이면 --> reserve status = 0 )
+		 
+		 3. product(prodNo) >  prodName, showTime
+		 
+		 4. notireq(prodNo, viewDate, section, status==1?)  >  결과 userNo 리스트   
+		 
+		 5. noti (결과 userNo 리스트 ) > 알림 번호(시퀀스), 내용 ( viewDate+showTime , prodName, section ) , 알림 시간 (sysdate)   
+		 								  
+		 								    └ 링크(예매페이지 --> 취소 좌석 선택 (prodNo, viewDate, grade, section, col, num 으로 기본 선택) )
+		 								    
+		 6. Users (결과 userNo 리스트) > email로 내용 전송								    
+		 
+		 */
+		
+		
+		
+	}
+	
 }
