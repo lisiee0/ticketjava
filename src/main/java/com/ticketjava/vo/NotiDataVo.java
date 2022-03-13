194d0fb66b696df2e456d810ab10ac7f47881fc1
이원준 <ggoong8@gmail.com>
2022-03-13 22:10:50 +0900

    클래스명 수정

---
+++ src/main/java/com/ticketjava/vo/NotiDataVo.java
@@ -0,0 +1,20 @@
+package com.ticketjava.vo;
+
+public class NotiDataVo {
+
+	// selseat
+	private int selseatNo;
+	private int rezNo;
+	
+	// reserve
+	private String grade;
+	private String section;
+	private int col;
+	private int num;
+	
+	// product
+	private int prodNo;
+	private String viewDate;
+	private String showTime;
+	
+}
