967491528f6726ce24bb547bd516869668f67022
Hojun_Yoo <lisiee0@naver.com>
2022-03-15 09:24:55 +0900

    main

--- src/main/java/com/ticketjava/controller/MainController.java
+++ src/main/java/com/ticketjava/controller/MainController.java
@@ -11,13 +11,10 @@ public class MainController {
 	
 	@RequestMapping(value="/", method={RequestMethod.GET, RequestMethod.POST })
 	public String main() {
-		System.out.println("MainController.main 메인 페이지");
+
 		return "main/index";
 	} // main
 	
-	@RequestMapping("search")
-	public String search() {
-		return "main/search";
-	} // search
+
 	
 } // The end of MainController
