46e3f0a503c87d46c503f5db41fdeeacfe9a75d8
copymachine <copymach@naver.com>
2022-03-16 21:03:00 +0900

    리뷰 추가

---
+++ src/main/java/com/ticketjava/vo/ReviewVo.java
@@ -0,0 +1,18 @@
+package com.ticketjava.vo;
+
+import java.util.List;
+
+public class ReviewVo {
+
+    private int reviewNo; // 후기번호
+    private int prodNo; // 공연번호
+    private int userNo; // 회원번호
+    private int rating; // 평점
+    private String content; // 내용
+    private String prodName; // 공연이름
+    private String regDate; // 작성일
+    
+
+	
+	
+} // The end of ReviewVo
