9c1d823832d38d119eaffbd8e7a748604d1c817b
Hojun_Yoo <lisiee0@naver.com>
2022-03-18 22:11:25 +0900

    notice

--- src/main/java/com/ticketjava/vo/NoticeVo.java
+++ src/main/java/com/ticketjava/vo/NoticeVo.java
@@ -7,66 +7,88 @@ public class NoticeVo {
 	private String content;
 	private String regDate;
 	private String theaterName;
+	private String homePage;
 	
 	
 	public NoticeVo() {
 		
 	}
-	
-	public NoticeVo(int noticeNo, int theaterNo, String content, String regDate, String theaterName) {
+
+
+	public NoticeVo(int noticeNo, int theaterNo, String content, String regDate, String theaterName, String homePage) {
 		this.noticeNo = noticeNo;
 		this.theaterNo = theaterNo;
 		this.content = content;
 		this.regDate = regDate;
 		this.theaterName = theaterName;
+		this.homePage = homePage;
 	}
 
-	
+
 	public int getNoticeNo() {
 		return noticeNo;
 	}
+
 
 	public void setNoticeNo(int noticeNo) {
 		this.noticeNo = noticeNo;
 	}
 
+
 	public int getTheaterNo() {
 		return theaterNo;
 	}
+
 
 	public void setTheaterNo(int theaterNo) {
 		this.theaterNo = theaterNo;
 	}
 
+
 	public String getContent() {
 		return content;
 	}
+
 
 	public void setContent(String content) {
 		this.content = content;
 	}
 
+
 	public String getRegDate() {
 		return regDate;
 	}
+
 
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
 
+
 	public String getTheaterName() {
 		return theaterName;
 	}
+
 
 	public void setTheaterName(String theaterName) {
 		this.theaterName = theaterName;
 	}
 
-	
+
+	public String getHomePage() {
+		return homePage;
+	}
+
+
+	public void setHomePage(String homePage) {
+		this.homePage = homePage;
+	}
+
+
 	@Override
 	public String toString() {
 		return "NoticeVo [noticeNo=" + noticeNo + ", theaterNo=" + theaterNo + ", content=" + content + ", regDate="
-				+ regDate + ", theaterName=" + theaterName + "]";
+				+ regDate + ", theaterName=" + theaterName + ", homePage=" + homePage + "]";
 	}
-	
+
 }
