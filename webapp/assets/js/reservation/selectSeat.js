ba34c8e35854e7aa0483f9ee4d23885799662c10
이원준 <ggoong8@gmail.com>
2022-03-23 01:24:17 +0900

    주석

--- webapp/assets/js/reservation/selectSeat.js
+++ webapp/assets/js/reservation/selectSeat.js
@@ -7,31 +7,12 @@ $(function(){
 			viewDate: viewDate ,
 			prodNo: prodNo
 	};
-	$.ajax({
-		url: pageContext+"/selseat/occupyList",
-		type : "post",
-		data : rezVo,
-		async : false,
-		dataType: "json",
-		success : function(selseatList){
-
-			for(var selseat of selseatList){
-				var grade = selseat.grade;
-				if(grade == 'vip')
-					grade = 'v';
-				var section = selseat.section;
-				var col = selseat.col;
-				var num = selseat.num;
-				
-				$('[class='+grade+'][data-section='+section+'][data-col='+col+'][data-num='+num+']').attr("disabled",true);
-			}
-			
-		},
-		error : function(XHR, status, error) {
-			console.error(status + " : " + error);
-		}
-	});
 	
+	/* 예매 좌석 가져오기*/
+	fetchOccupy(rezVo);
+	
+	
+	/* 잔여석 계산 */
 	var gradeList = $('#priceTable tr');
 	for(var i=0; i<gradeList.length; i++){
 		var gradetr = gradeList.eq(i);
@@ -40,13 +21,11 @@ $(function(){
 			grade = 'v';
 		}
 		
-		var notOccupy = $('#seatPlanDiv input[type=checkbox].'+grade+':not(:disabled)');
-		var remaining = notOccupy.length;
-		
+		var remaining = $('#seatPlanDiv input[type=checkbox].'+grade+':not(:disabled)').length;
 		gradetr.find('.remaining').text(remaining+'석');
 	}
 	
-	
+	/* 좌석 선택 시 선택 좌석 출력 */
 	$('#seatPlanDiv').on('change','table td input[type=checkbox]' ,function(){
 		if(this.checked){
 			var grade= $(this).attr("class");
@@ -60,19 +39,18 @@ $(function(){
 					'<tr id="tr'+$(this).attr('id')+'"><td>'+grade.toUpperCase()+
 					'석</td><td class="right">'+section+'구역 '+col+'열 '+num+'번</td></tr>'
 			);
-			
 		}
 	});
 	
-	
+	/* 선택 취소 시 선택 좌석에서 삭제 */
 	$('#seatPlanDiv').on('change','table td input[type=checkbox]' ,function(){
 		if(! this.checked){
 			$('#tr'+$(this).attr('id')).remove();
 		}
-		
-		
 	});
 	
+	
+	/* 다음 단계 버튼 클릭 시 */
 	$('#nextBtn').on('click', function(){
 		
 		var chkbox = $('[type=checkbox]:checked').get();
@@ -82,6 +60,7 @@ $(function(){
 			return false;
 		}
 		
+		/* 선택 좌석 리스트 */
 		var selseatList = [];
 		for(var i=0; i< chkbox.length; i++) {
 			var e = $('[type=checkbox]:checked').eq(i);
@@ -108,7 +87,45 @@ $(function(){
 				selseatList:selseatList
 		};
 		
+		/* 좌석 선점 */
+		preoccupy(rezVo);
 		
+	});
+	
+	/* 취소표 알림 페이지 이동 */
+	$('#cancelNotiBtn').on('click',function(){
+		$('#seatForm').attr('action', pageContext+'/notireq/notireq');
+		$('#seatForm').submit();
+	});
+	
+	
+	function fetchOccupy(rezVo) {
+		 $.ajax({
+			url: pageContext+"/selseat/occupyList",
+			type : "post",
+			data : rezVo,
+			async : false,
+			dataType: "json",
+			success : function(selseatList){
+				for(var selseat of selseatList){
+					var grade = selseat.grade;
+					if(grade == 'vip')
+						grade = 'v';
+					var section = selseat.section;
+					var col = selseat.col;
+					var num = selseat.num;
+					
+					$('[class='+grade+'][data-section='+section+'][data-col='+col+'][data-num='+num+']').attr("disabled",true);
+				}
+			},
+			error : function(XHR, status, error) {
+				console.error(status + " : " + error);
+			}
+		});
+	};
+	
+	
+	function preoccupy(rezVo){
 		$.ajax({
 			url: pageContext+"/reservation/preoccupy",
 			type : "post",
@@ -117,8 +134,7 @@ $(function(){
 			data: JSON.stringify(rezVo),
 			dataType: "json",
 			success : function(rezNo){
-				console.log(rezNo);
-				if(rezNo == -1) {
+				if(rezNo == -1) {	// 다음 단계 버튼을 누르기 전에 좌석이 예매 or 선점된 경우
 					alert('예약된 좌석이 포함돼있습니다.');
 					location.reload();
 					return false;
@@ -130,12 +146,7 @@ $(function(){
 				console.error(status + " : " + error);
 			}
 		});
-		 
-		
-	});
+	}
 	
-	$('#cancelNotiBtn').on('click',function(){
-		$('#seatForm').attr('action', pageContext+'/notireq/notireq');
-		$('#seatForm').submit();
-	});
+	
 });
