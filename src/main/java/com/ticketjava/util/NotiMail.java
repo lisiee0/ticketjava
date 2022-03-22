6eee9880408086e82a57acca9048e8129034c663
이원준 <ggoong8@gmail.com>
2022-03-22 16:25:48 +0900

    메일 제목 수정

--- src/main/java/com/ticketjava/util/NotiMail.java
+++ src/main/java/com/ticketjava/util/NotiMail.java
@@ -7,7 +7,7 @@ import com.ticketjava.vo.NotiDataVo;
 public class NotiMail {
 
 	public static void sendMail(List<String> emailList, NotiDataVo notiDataVo) {
-		String title ="티켓 자바" + notiDataVo.getProdName()+ " 취소표 안내"; 
+		String title ="티켓 자바 " + notiDataVo.getProdName()+ " 취소표 안내"; 
 	    String contents = "[취소표 안내] <br>"
 				+ notiDataVo.getViewDate()+" "+notiDataVo.getShowTime()+" "+notiDataVo.getProdName()+" "+notiDataVo.getGrade().toUpperCase()+"석 "
 				+ notiDataVo.getSection()+"구역 "+notiDataVo.getCol()+"열 "+notiDataVo.getNum()+"번 좌석 취소 안내 <br>"
