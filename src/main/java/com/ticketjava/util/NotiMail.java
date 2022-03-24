de91c7b2033d004782ef0ef0b8b23a2267392bc9
이원준 <ggoong8@gmail.com>
2022-03-25 00:10:25 +0900

    localhost

--- src/main/java/com/ticketjava/util/NotiMail.java
+++ src/main/java/com/ticketjava/util/NotiMail.java
@@ -11,7 +11,7 @@ public class NotiMail {
 	    String contents = "[취소표 안내] <br>"
 				+ notiDataVo.getViewDate()+" "+notiDataVo.getShowTime()+" "+notiDataVo.getProdName()+" "+notiDataVo.getGrade().toUpperCase()+"석 "
 				+ notiDataVo.getSection()+"구역 "+notiDataVo.getCol()+"열 "+notiDataVo.getNum()+"번 좌석 취소 안내 <br>"
-				+ "<a href='http://localhost:8088/ticketjava/product/info?prodNo="+notiDataVo.getProdNo()+"&viewDate="+notiDataVo.getViewDate()+"'>예매 바로가기 링크</a>";
+				+ "<a href='http://127.0.0.1:8088/ticketjava/product/info?prodNo="+notiDataVo.getProdNo()+"&viewDate="+notiDataVo.getViewDate()+"'>예매 바로가기 링크</a>";
 		
 		JavaMail.sendMail(emailList, title ,contents);
 		
